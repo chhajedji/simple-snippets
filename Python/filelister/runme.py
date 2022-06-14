099cd917c649781a4cc0c17c847f87ed46cdd25e
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2022-06-13 21:44:16 -0400

    Removed unnecessary parts.

--- Python/filelister/runme.py
+++ Python/filelister/runme.py
@@ -15,16 +15,16 @@ def list_files(base_dir):
     for entry in os.listdir(base_dir):
         if os.path.isdir(os.path.join(base_dir, entry)):
 
-            all_files = list_files(os.path.join(base_dir, entry))
+            list_files(os.path.join(base_dir, entry))
         elif os.path.isfile(os.path.join(base_dir, entry)):
             # print(entry, " is a file.")
             # print("Found a file: ", entry, "\tAppending: ", os.path.join(base_dir, entry))
             all_files.append(os.path.join(base_dir, entry))
         # print("****\nentry: {}\nall_files: {}\n****\n".format(entry, all_files))
-    return all_files
+    # return all_files
 
 
-all_files = list_files(cwd)
+list_files(cwd)
 print("==========")
 for entry in all_files:
     print(entry)
