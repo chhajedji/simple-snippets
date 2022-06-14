0c77a6f73c7416807d09c973e856d1ddce7264f3
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2022-06-13 21:15:07 -0400

    Minor customizations.

--- Python/filelister/runme.py
+++ Python/filelister/runme.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-import os
+import os, sys
 
-cwd = './'
+if len(sys.argv) > 1:
+    cwd = sys.argv[1]
+else:
+    cwd = './'
 
 all_files = []
 
@@ -10,7 +13,6 @@ def list_files(base_dir):
     global all_files
     # print("base_dir: ", base_dir)
     for entry in os.listdir(base_dir):
-        # print("entry: ", entry)
         if os.path.isdir(os.path.join(base_dir, entry)):
 
             all_files = list_files(os.path.join(base_dir, entry))
@@ -18,9 +20,12 @@ def list_files(base_dir):
             # print(entry, " is a file.")
             # print("Found a file: ", entry, "\tAppending: ", os.path.join(base_dir, entry))
             all_files.append(os.path.join(base_dir, entry))
+        # print("****\nentry: {}\nall_files: {}\n****\n".format(entry, all_files))
     return all_files
 
 
 all_files = list_files(cwd)
+print("==========")
 for entry in all_files:
     print(entry)
+print("==========")
