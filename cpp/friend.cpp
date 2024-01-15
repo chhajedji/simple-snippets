8c31859c30f193aa29e06ac1be5ff22d260bdb2c
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2024-01-15 16:42:25 -0500

    Friend function.

---
+++ cpp/friend.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// Friend functions
+
+using namespace std;
+
+class B;
+
+class A{
+    private:
+        int data_a;
+    public:
+        A(int x){
+            data_a=x;
+        }
+        friend int fun(A, B);
+};
+
+class B{
+    private:
+        int data_b;
+    public:
+        B(int x) {
+            data_b=x;
+        }
+        friend int fun(A, B);
+};
+
+int fun(A a, B b){
+    return a.data_a+b.data_b;
+}
+int main(){
+    A a(10);
+    B b(20);
+    cout<<fun(a,b)<<endl;
+    return 0;
+}
