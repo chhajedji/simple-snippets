af179c95ab002631790fed0871e5a2f6b75cd41c
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2024-08-16 16:15:57 -0400

    CPP memmove: Remove errors.

--- cpp/move.c
+++ cpp/move.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void move1(uint8_t* src, uint32_t len, uint8_t* dest)
+void move1(uint8_t* src, uint32_t bytes, uint8_t* dest)
 {
 	printf("Move 1");
-	uint8_t *buf = (uint8_t *)malloc(sizeof(len));
+	uint8_t *buf = (uint8_t *)malloc(bytes);
 
-	for (int i = 0; i < (uint32_t)(len/sizeof(uint8_t)); i++) {
+	for (uint32_t i = 0; i < (uint32_t)(bytes); i++) {
 		buf[i] = src[i];
 	}
 
-	for (int i = 0; i < (uint32_t)(len/sizeof(uint8_t)); i++) {
+	for (uint32_t i = 0; i < (uint32_t)(bytes); i++) {
 		dest[i] = buf[i];
 	}
 
@@ -36,18 +36,18 @@ void move2(uint8_t* src, uint32_t bytes, uint8_t* dest)
 
 void main()
 {
-	int a[5] = {1, 2, 3, 4, 5};
-	int b[5];
-	int c[5];
-	move1(&a, 5*sizeof(int), &c);
-	move2(&a, 5*sizeof(int), &b);
+	int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int b[10];
+	int c[10];
+	move1(&a, 10*sizeof(int), &c);
+	move2(&a, 10*sizeof(int), &b);
 
-	for (int i = 0; i < 5; i += 1) {
+	for (int i = 0; i < 10; i += 1) {
 		printf("b[%d]: %d\t", i, b[i]);
 	}
 	printf("\n");
 
-	for (int i = 0; i < 5; i += 1) {
+	for (int i = 0; i < 10; i += 1) {
 		printf("c[%d]: %d\t", i, c[i]);
 	}
 	printf("\n");
