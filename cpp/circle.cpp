de89d2cbfe4537ec693f868c6515068ff9a40960
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2024-10-01 23:28:40 -0700

    CPP code to draw a circle of defined radius.

---
+++ cpp/circle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+void draw(int);
+
+int main()
+{
+	draw(20);
+	return 0;
+}
+
+void draw(int r)
+{
+	int x, y;
+	std::cout << "Radius: " << r << std::endl;
+
+	for (y = 2*r; y >= 0; y--) {
+		// Getting x coordinate by solving for equation:
+		// (x-r)^2 + (y-r)^2 = r^2
+		// Center at (r, r).
+		x = (int)(2*r - (r + sqrt(y*(2*r - y))));
+
+		for (int j = 0; j < x; j++) {
+			std::cout << "  ";
+		}
+		std::cout << "X";
+
+		for (int i = 0; i < (2*r - 2 * x); i++) {
+			std::cout << "  ";
+		}
+		std::cout << "X\n";
+	}
+}
