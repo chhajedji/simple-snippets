9249b628310b8c582c729766941730faeba267a8
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2024-10-22 14:21:22 -0700

    Notes in comment for referernce.
    Who runs the code just to check the notes in scale.

--- Python/music_notes.py
+++ Python/music_notes.py
@@ -1,5 +1,41 @@
 #!/usr/bin/env python3
 
+
+# Fancy way to print out the following:
+
+
+# Scale    Sa    Re    Ga    Ma    Pa    Dha    Ni    Sa
+
+# C        C     D     E     F     G     A      B     C
+# C#       C#    D#    F     F#    G#    A#     C     C#
+# D        D     E     F#    G     A     B      C#    D
+# D#       D#    F     G     G#    A#    C      D     D#
+# E        E     F#    G#    A     B     C#     D#    E
+# F        F     G     A     A#    C     D      E     F
+# F#       F#    G#    A#    B     C#    D#     F     F#
+# G        G     A     B     C     D     E      F#    G
+# G#       G#    A#    C     C#    D#    F      G     G#
+# A        A     B     C#    D     E     F#     G#    A
+# A#       A#    C     D     D#    F     G      A     A#
+# B        B     C#    D#    E     F#    G#     A#    B
+
+# Flat Notes:
+
+# Scale    Sa    Re    Ga    Ma    Pa    Dha    Ni    Sa
+
+# C        C     D     E     F     G     A      B     C
+# D♭       D♭    E♭    F     G♭    A♭    B♭     C     D♭
+# D        D     E     G♭    G     A     B      D♭    D
+# E♭       E♭    F     G     A♭    B♭    C      D     E♭
+# E        E     G♭    A♭    A     B     D♭     E♭    E
+# F        F     G     A     B♭    C     D      E     F
+# G♭       G♭    A♭    B♭    B     D♭    E♭     F     G♭
+# G        G     A     B     C     D     E      G♭    G
+# A♭       A♭    B♭    C     D♭    E♭    F      G     A♭
+# A        A     B     D♭    D     E     G♭     A♭    A
+# B♭       B♭    C     D     E♭    F     G      A     B♭
+# B        B     D♭    E♭    E     G♭    A♭     B♭    B
+
 sharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 flat  = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
 
